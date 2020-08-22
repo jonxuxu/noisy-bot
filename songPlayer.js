c392c10d41758b843b42cee40d1019b5d883aabd
Jonathan <jonathanxu25@gmail.com>
2020-08-22 16:20:26 -0400

    changed songplayer

--- songPlayer.js
+++ songPlayer.js
@@ -1,23 +1,19 @@
-
 const startSong = (connection, file) => {
+  // highWaterMark: used for
+  const dispatcher = connection.play(file, { highWaterMark: 50 });
 
-    const file = file
+  // TODO: improve stability and quality with opus
+  dispatcher.on("start", () => {
+    console.log(`${file} is now playing!`);
+  });
 
-    // highWaterMark: used for 
-    const dispatcher = connection.play(file, { highWaterMark: 50 })
+  dispatcher.on("finish", () => {
+    console.log(`${file} has finished playing!`);
+    connection.disconnect();
+  });
 
-    // TODO: improve stability and quality with opus
-    dispatcher.on('start', () => {
-      console.log(`${file} is now playing!`);
-    });
-    
-    dispatcher.on('finish', () => {
-      console.log(`${file} has finished playing!`);
-      connection.disconnect();
-    });
-    
-    // Always remember to handle errors appropriately!
-    dispatcher.on('error', console.error);
-}
+  // Always remember to handle errors appropriately!
+  dispatcher.on("error", console.error);
+};
 
 exports.startSong = startSong;
