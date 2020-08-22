ff1143a4caa86632832e506050b17986d67affa2
Ricky Mao <rickyrmao@gmail.com>
2020-08-22 13:15:40 -0700

    Abstracted audio player

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 var Discord = require("discord.js");
 var logger = require("winston");
 var auth = require("./auth.json");
+const { OpusEncoder } = require('@discordjs/opus');
+var songPlayer = require('./songPlayer')
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console(), {
@@ -10,6 +12,16 @@ logger.level = "debug";
 // Initialize Discord Bot
 var bot = new Discord.Client();
 bot.on("ready", () => {
+
+  
+  // // Create the encoder.
+  // // Specify 48kHz sampling rate and 2 channel size.
+  // const encoder = new OpusEncoder(48000, 2);
+  
+  // // Encode and decode.
+  // const encoded = encoder.encode(buffer, 48000 / 100);
+  // const decoded = encoder.decode(encoded, 48000 / 100);
+
   logger.info("Connected");
   logger.info("Logged in as: ");
   logger.info(bot.username + " - (" + bot.id + ")");
@@ -22,9 +34,14 @@ bot.on("message", async (message) => {
     return
   }
 
-  // Joins voice channel
+  // Joins voice channel and plays music
   if ( message.content == "!join" && message.member.voice.channel) {
     const connection = await message.member.voice.channel.join()
+
+    songPlayer.startSong(connection, "test.ogg")
+
+
+
   }
 
   if (message.content.substring(0, 1) == "!") {
---
+++ songPlayer.js
@@ -0,0 +1,23 @@
+
+const startSong = (connection, file) => {
+
+    const file = file
+
+    // highWaterMark: used for 
+    const dispatcher = connection.play(file, { highWaterMark: 50 })
+
+    // TODO: improve stability and quality with opus
+    dispatcher.on('start', () => {
+      console.log(`${file} is now playing!`);
+    });
+    
+    dispatcher.on('finish', () => {
+      console.log(`${file} has finished playing!`);
+      connection.disconnect();
+    });
+    
+    // Always remember to handle errors appropriately!
+    dispatcher.on('error', console.error);
+}
+
+exports.startSong = startSong;
