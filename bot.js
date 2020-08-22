f62ba18dda110d81809138db7221d57f7911ac04
Ricky Mao <rickyrmao@gmail.com>
2020-08-22 11:03:45 -0700

    Changed to discord.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-var Discord = require("discord.io");
+var Discord = require("discord.js");
 var logger = require("winston");
 var auth = require("./auth.json");
 // Configure logger settings
@@ -8,32 +8,33 @@ logger.add(new logger.transports.Console(), {
 });
 logger.level = "debug";
 // Initialize Discord Bot
-var bot = new Discord.Client({
-  token: auth.token,
-  autorun: true,
-});
-bot.on("ready", function (evt) {
+var bot = new Discord.Client();
+bot.on("ready", () => {
   logger.info("Connected");
   logger.info("Logged in as: ");
   logger.info(bot.username + " - (" + bot.id + ")");
 });
-bot.on("message", function (user, userID, channelID, message, evt) {
+bot.on("message", (message) => {
   // Our bot needs to know if it will execute a command
   // It will listen for messages that will start with `!`
-  if (message.substring(0, 1) == "!") {
-    var args = message.substring(1).split(" ");
+
+  if (!message.content) {
+    return
+  }
+
+  if (message.content.substring(0, 1) == "!") {
+    var args = message.content.substring(1).split(" ");
     var cmd = args[0];
 
     args = args.splice(1);
     switch (cmd) {
       // !ping
       case "ping":
-        bot.sendMessage({
-          to: channelID,
-          message: "Pong!",
-        });
+        message.channel.send('pong!');
         break;
       // Just add any case commands if you want to..
     }
   }
 });
+
+bot.login(auth.token)
