a071d451d236d3728f4b275c7b0dbbba6e9c1ba6
Jonathan <jonathanxu25@gmail.com>
2020-10-25 17:52:38 -0600

    added more functionality and aesthetics

--- bot.js
+++ bot.js
@@ -15,6 +15,45 @@ bot.on("ready", () => {
   logger.info("Logged in as: ");
   logger.info(bot.username + " - (" + bot.id + ")");
 });
+
+// Embeds for message replies
+const helpEmbed = new Discord.MessageEmbed()
+  .setColor("#ff00c1")
+  .setTitle("Noisy Help")
+  .setURL("https://noisy.live")
+  .setDescription(
+    ":checkered_flag: Click [here](https://noisy.live#commands) for a list of commands \n :question: New to Noisy? [Check us out!](https://noisy.live)"
+  );
+const currPlayEmbed = (genre, requester) =>
+  new Discord.MessageEmbed()
+    .setColor("#ff00c1")
+    .setTitle(genre)
+    .setAuthor(
+      "Now playing",
+      "https://i.ibb.co/M7916b9/favicon.png",
+      "https://noisy.live"
+    )
+    .setDescription(`Requested by ${requester}`)
+    .setThumbnail("https://i.imgur.com/wSTFkRM.png");
+
+// Play functions
+var connection = null;
+var currGenre = "pop";
+const play = (channel, genre = []) => {
+  if (genre.length === 0) {
+    channel.send(":notes: Generating and playing live `pop` music by default");
+    songPlayer.startSong(
+      connection,
+      "https://noisy-s3.s3.ca-central-1.amazonaws.com/out/Soulful.ogg"
+    );
+  } else {
+    currGenre = genre[0];
+    channel.send(
+      `:notes: Generating and playing live \`${currGenre}\` music by default`
+    );
+  }
+};
+
 bot.on("message", async (message) => {
   // Our bot needs to know if it will execute a command
   // It will listen for messages that will start with `!`
@@ -28,46 +67,76 @@ bot.on("message", async (message) => {
     var cmd = args[0];
 
     args = args.splice(1);
-    switch (cmd) {
-      // !ping
-      case "ping":
-        message.channel.send("pong!");
-        break;
-      // Joins voice channel
-      case "join":
-        if (message.member.voice.channel) {
-          const connection = await message.member.voice.channel.join();
-          songPlayer.startSong(
-            connection,
-            "https://noisy-s3.s3.ca-central-1.amazonaws.com/out/Soulful.ogg"
+    // !ping
+    if (cmd === "ping") {
+      message.channel.send("pong!");
+    }
+    // Joins voice channel
+    else if (cmd === "join" || cmd === "play") {
+      if (message.member.voice.channel) {
+        if (message.guild.me.voice.channel) {
+          if (cmd === "join") {
+            message.reply(
+              `I'm already connected to :sound:\`${message.guild.me.voice.channel.name}\``
+            );
+          } else {
+            play(message.channel, args);
+          }
+        } else {
+          connection = await message.member.voice.channel.join();
+          message.channel.send(
+            `:thumbsup: Joined \`${message.guild.me.voice.channel.name}\``
           );
-        } else {
-          message.channel.send("You must be in a voice channel!");
+          if (cmd === "join") {
+            play(message.channel);
+          } else {
+            play(message.channel, args);
+          }
         }
-        break;
-      // Leave the voice channel
-      case "leave":
-        if (message.guild.me.voice.channel) {
-          message.guild.me.voice.channel.leave();
-          message.reply("I have successfully left the voice channel!");
-        } else {
-          message.reply("I'm not connected to a voice channel!");
-        }
-        break;
-      case "pause":
-        if (message.guild.me.voice.channel) {
-          songPlayer.pauseSong();
-        } else {
-          message.reply("I'm not connected to a voice channel!");
-        }
-
-        break;
-      case "resume":
-        if (message.guild.me.voice.channel) {
-          songPlayer.resumeSong();
-        } else {
-          message.reply("I'm not connected to a voice channel!");
-        }
+      } else {
+        message.channel.send(":exclamation: You must be in a voice channel!");
+      }
+    }
+    // Leave the voice channel
+    else if (cmd === "leave") {
+      if (message.guild.me.voice.channel) {
+        message.guild.me.voice.channel.leave();
+        message.channel.send(":leaves: Successfully Disconnected");
+      } else {
+        message.reply("I'm not connected to a voice channel!");
+      }
+    }
+    // Pauses the currently playing song
+    else if (cmd === "pause") {
+      if (message.guild.me.voice.channel) {
+        songPlayer.pauseSong();
+        message.channel.send(":pause_button: Paused");
+      } else {
+        message.reply("I'm not connected to a voice channel!");
+      }
+    }
+    // Resumes the currently paused song
+    else if (cmd === "resume") {
+      if (message.guild.me.voice.channel) {
+        songPlayer.resumeSong();
+        message.channel.send(":play_pause: Resuming");
+      } else {
+        message.reply("I'm not connected to a voice channel!");
+      }
+    }
+    // Now playing
+    else if (cmd === "np") {
+      if (message.guild.me.voice.channel) {
+        message.channel.send(currPlayEmbed(currGenre, "person"));
+      } else {
+        message.channel.send(
+          ":cry: I'm not connected to a voice channel. Type `!join` to get me in one"
+        );
+      }
+    }
+    // Displays a help message
+    else if (cmd === "help") {
+      message.channel.send(helpEmbed);
     }
   }
 });
