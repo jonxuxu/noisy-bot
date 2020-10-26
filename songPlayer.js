448094f1dc914d316f2058e31b98f5d5391e91c5
Jonathan <jonathanxu25@gmail.com>
2020-10-25 18:07:55 -0600

    auth

--- songPlayer.js
+++ songPlayer.js
@@ -1,50 +1,41 @@
-
-class songPlayer  {
-
+class songPlayer {
   constructor() {
-    this.dispatcher = null
+    this.dispatcher = null;
   }
-
 
   startSong = (connection, file) => {
     // highWaterMark: used for
     this.dispatcher = connection.play(file, { highWaterMark: 50 });
-  
+
     // TODO: improve stability and quality with opus
     this.dispatcher.on("start", () => {
       console.log(`${file} is now playing!`);
     });
-  
+
     this.dispatcher.on("finish", () => {
       console.log(`${file} has finished playing!`);
       connection.disconnect();
     });
-  
+
     // Always remember to handle errors appropriately!
     this.dispatcher.on("error", console.error);
   };
 
-
-    pauseSong = () => {
-
-      if (!this.dispatcher) {
-        throw "Error: no audio playing";
-      }
-
-      this.dispatcher.pause();
-
+  pauseSong = () => {
+    if (!this.dispatcher) {
+      throw "Error: no audio playing";
     }
 
-    resumeSong = () => {
-      if (!this.dispatcher) {
-        throw "Error: no audio playing";
-      }
+    this.dispatcher.pause();
+  };
 
-      this.dispatcher.resume();
+  resumeSong = () => {
+    if (!this.dispatcher) {
+      throw "Error: no audio playing";
     }
 
+    this.dispatcher.resume();
+  };
 }
-
-
 
 module.exports = new songPlayer();
